df081608e1f5fe6a9c0e2bd2fa68377d21e37ef9
Jun Beom Park <94429684+JunBeoPark@users.noreply.github.com>
2023-07-21 01:34:53 +0900

    Update and rename README.md to Cal1.kt

---
+++ Cal1.kt
@@ -0,0 +1,28 @@
+# nbcamp_cal
+
+fun main() {
+
+}
+
+class Calculator
+var num1 = readLine()!!.toInt()
+var num2 = readLine()!!.toInt()
+
+var addOption = num1 + num2
+var substractOption = num1 - num2
+var multiplyOption = num * num2
+var divideOption = num1 / num2
+
+var result
+
+if(result == addOption) {
+	println("${num1} + ${num2} = ${addOption}")
+} else if(result == substractOption) {
+  println("${num1} - ${num2} = ${substractOption}")
+} else if(result == multiplyOption) {
+	println("${num1} * ${num2} = ${multiplyOption}")
+} else if(result == divideOption) {
+	println("${num1} / ${num2} = ${divideOption}")
+} else {
+	println("Error")
+}
