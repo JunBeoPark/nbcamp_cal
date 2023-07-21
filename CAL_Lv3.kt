3d91f55d98a1bd349cc3fa0defba252218600a38
Jun Beom Park <94429684+JunBeoPark@users.noreply.github.com>
2023-07-21 10:24:06 +0900

    Update CAL_Lv3.kt

--- CAL_Lv3.kt
+++ CAL_Lv3.kt
@@ -1,12 +1,36 @@
 
 fun main() {
     val calc = Calculator()
-    println("1 더하기 2 결과는 : ${calc.addOperation(AddOperation(), 1, 2)} 입니다")
+    var num1
+    var num2
+    
+    println("${num1} 더하기 ${num2} 결과는 : ${calc.addOperation(AddOperation(), num1, num2)} 입니다")
+    println("${num1} 뺴기 ${num2} 결과는 : ${calc.substractOperation(SubstractOperation(), num1, num2)} 입니다")
+    println("${num1} 곱하기 ${num2} 결과는 : ${calc.multiplyOperation(MultiplyOperation(), num1, num2)} 입니다")
+    println("${num1} 나누기 ${num2} 결과는 : ${calc.divideOperation(DivideOperation(), num1, num2)} 입니다")
 }
 
 class Calculator {
     fun addOperation(num1: Int, num2: Int): Double {
         var num3 = num1 + num2
+        
+        return num3
+    }
+
+    fun substractOperation(num1: Int, num2: Int): Double {
+        var num3 = num1 - num2
+        
+        return num3
+    }
+
+        fun multiplyOperation(num1: Int, num2: Int): Double {
+        var num3 = num1 * num2
+        
+        return num3
+    }
+
+    fun divideOperation(num1: Int, num2: Int): Double {
+        var num3 = num1 / num2
         
         return num3
     }
